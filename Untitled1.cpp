50c133aaf708ea4608bae0d78f844e9c74efd453
erenhalid <95280523+erenhalid@users.noreply.github.com>
2021-12-07 20:45:10 +0300

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+	
+	int i,a,j;
+	printf("bir sayi giriniz:");
+	scanf("%d",&a);
+	
+	for(i=a;i>0;i--){
+	
+		for(j=a;j>0;j--){
+	
+			printf("%d\n",a);
+				printf("\r");
+				
+		}
+		a--;
+	
+		
+	
+	
+	
+	
+	
+	}return 0;
+}
